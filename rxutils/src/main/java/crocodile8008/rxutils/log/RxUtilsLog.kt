f4115bbd04d3168fae47e4c60dc551aca892bb63
crocodile8 <crocodile8000@gmail.com>
2021-08-26 01:11:31 +0300

    RxUtilsLog error logging

--- rxutils/src/main/java/crocodile8008/rxutils/log/RxUtilsLog.kt
+++ rxutils/src/main/java/crocodile8008/rxutils/log/RxUtilsLog.kt
@@ -6,10 +6,25 @@ object RxUtilsLog {
 
     var target = RxUtilsLogTarget.EMPTY
 
+    private val tag = "RxUtilsLog"
+
     fun d(msg: String) {
         when (target) {
             RxUtilsLogTarget.CONSOLE -> println(msg)
-            RxUtilsLogTarget.ANDROID_LOG -> Log.d("RxUtilsLog", msg)
+            RxUtilsLogTarget.ANDROID_LOG -> Log.d(tag, msg)
+            else -> { /* Empty */ }
+        }
+    }
+
+    fun e(msg: String = "", throwable: Throwable) {
+        when (target) {
+            RxUtilsLogTarget.CONSOLE -> {
+                println(msg)
+                throwable.printStackTrace()
+            }
+            RxUtilsLogTarget.ANDROID_LOG -> {
+                Log.e(tag, msg, throwable)
+            }
             else -> { /* Empty */ }
         }
     }
