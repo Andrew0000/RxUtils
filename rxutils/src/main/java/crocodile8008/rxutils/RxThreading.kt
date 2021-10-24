bc1924f29a540f8c704e48c6622d9e917761bed8
crocodile8 <crocodile8000@gmail.com>
2021-10-24 17:33:12 +0300

    Minor formatting

--- rxutils/src/main/java/crocodile8008/rxutils/RxThreading.kt
+++ rxutils/src/main/java/crocodile8008/rxutils/RxThreading.kt
@@ -10,57 +10,41 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 
 // Observable
 
-fun <T : Any> Observable<T>.fromComputation(): Observable<T> = this
-    .subscribeOn(Schedulers.computation())
+fun <T : Any> Observable<T>.fromComputation(): Observable<T> = subscribeOn(Schedulers.computation())
 
-fun <T : Any> Observable<T>.toComputation(): Observable<T> = this
-    .observeOn(Schedulers.computation())
+fun <T : Any> Observable<T>.toComputation(): Observable<T> = observeOn(Schedulers.computation())
 
-fun <T : Any> Observable<T>.fromIo(): Observable<T> = this
-    .subscribeOn(Schedulers.io())
+fun <T : Any> Observable<T>.fromIo(): Observable<T> = subscribeOn(Schedulers.io())
 
-fun <T : Any> Observable<T>.toIo(): Observable<T> = this
-    .observeOn(Schedulers.io())
+fun <T : Any> Observable<T>.toIo(): Observable<T> = observeOn(Schedulers.io())
 
 // Single
 
-fun <T : Any> Single<T>.fromComputation(): Single<T> = this
-    .subscribeOn(Schedulers.computation())
+fun <T : Any> Single<T>.fromComputation(): Single<T> = subscribeOn(Schedulers.computation())
 
-fun <T : Any> Single<T>.toComputation(): Single<T> = this
-    .observeOn(Schedulers.computation())
+fun <T : Any> Single<T>.toComputation(): Single<T> = observeOn(Schedulers.computation())
 
-fun <T : Any> Single<T>.fromIo(): Single<T> = this
-    .subscribeOn(Schedulers.io())
+fun <T : Any> Single<T>.fromIo(): Single<T> = subscribeOn(Schedulers.io())
 
-fun <T : Any> Single<T>.toIo(): Single<T> = this
-    .observeOn(Schedulers.io())
+fun <T : Any> Single<T>.toIo(): Single<T> = observeOn(Schedulers.io())
 
 // Completable
 
-fun Completable.fromComputation(): Completable = this
-    .subscribeOn(Schedulers.computation())
+fun Completable.fromComputation(): Completable = subscribeOn(Schedulers.computation())
 
-fun Completable.toComputation(): Completable = this
-    .observeOn(Schedulers.computation())
+fun Completable.toComputation(): Completable = observeOn(Schedulers.computation())
 
-fun Completable.fromIo(): Completable = this
-    .subscribeOn(Schedulers.io())
+fun Completable.fromIo(): Completable = subscribeOn(Schedulers.io())
 
-fun Completable.toIo(): Completable = this
-    .observeOn(Schedulers.io())
+fun Completable.toIo(): Completable = observeOn(Schedulers.io())
 
 // Maybe
 
-fun <T : Any> Maybe<T>.fromComputation(): Maybe<T> = this
-    .subscribeOn(Schedulers.computation())
+fun <T : Any> Maybe<T>.fromComputation(): Maybe<T> = subscribeOn(Schedulers.computation())
 
-fun <T : Any> Maybe<T>.toComputation(): Maybe<T> = this
-    .observeOn(Schedulers.computation())
+fun <T : Any> Maybe<T>.toComputation(): Maybe<T> = observeOn(Schedulers.computation())
 
-fun <T : Any> Maybe<T>.fromIo(): Maybe<T> = this
-    .subscribeOn(Schedulers.io())
+fun <T : Any> Maybe<T>.fromIo(): Maybe<T> = subscribeOn(Schedulers.io())
 
-fun <T : Any> Maybe<T>.toIo(): Maybe<T> = this
-    .observeOn(Schedulers.io())
+fun <T : Any> Maybe<T>.toIo(): Maybe<T> = observeOn(Schedulers.io())
 
