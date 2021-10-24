b28a0c47a83e8aff23ba21896983e6756faefdf7
crocodile8 <crocodile8000@gmail.com>
2021-10-24 17:22:37 +0300

    Added more threading function

--- rxutils/src/main/java/crocodile8008/rxutils/RxThreading.kt
+++ rxutils/src/main/java/crocodile8008/rxutils/RxThreading.kt
@@ -2,11 +2,65 @@
 
 package crocodile8008.rxutils
 
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Maybe
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
+
+// Observable
+
+fun <T : Any> Observable<T>.fromComputation(): Observable<T> = this
+    .subscribeOn(Schedulers.computation())
 
 fun <T : Any> Observable<T>.toComputation(): Observable<T> = this
     .observeOn(Schedulers.computation())
 
+fun <T : Any> Observable<T>.fromIo(): Observable<T> = this
+    .subscribeOn(Schedulers.io())
+
 fun <T : Any> Observable<T>.toIo(): Observable<T> = this
     .observeOn(Schedulers.io())
+
+// Single
+
+fun <T : Any> Single<T>.fromComputation(): Single<T> = this
+    .subscribeOn(Schedulers.computation())
+
+fun <T : Any> Single<T>.toComputation(): Single<T> = this
+    .observeOn(Schedulers.computation())
+
+fun <T : Any> Single<T>.fromIo(): Single<T> = this
+    .subscribeOn(Schedulers.io())
+
+fun <T : Any> Single<T>.toIo(): Single<T> = this
+    .observeOn(Schedulers.io())
+
+// Completable
+
+fun Completable.fromComputation(): Completable = this
+    .subscribeOn(Schedulers.computation())
+
+fun Completable.toComputation(): Completable = this
+    .observeOn(Schedulers.computation())
+
+fun Completable.fromIo(): Completable = this
+    .subscribeOn(Schedulers.io())
+
+fun Completable.toIo(): Completable = this
+    .observeOn(Schedulers.io())
+
+// Maybe
+
+fun <T : Any> Maybe<T>.fromComputation(): Maybe<T> = this
+    .subscribeOn(Schedulers.computation())
+
+fun <T : Any> Maybe<T>.toComputation(): Maybe<T> = this
+    .observeOn(Schedulers.computation())
+
+fun <T : Any> Maybe<T>.fromIo(): Maybe<T> = this
+    .subscribeOn(Schedulers.io())
+
+fun <T : Any> Maybe<T>.toIo(): Maybe<T> = this
+    .observeOn(Schedulers.io())
+
--- rxutils/src/main/java/crocodile8008/rxutils/android/RxAndroidThreading.kt
+++ rxutils/src/main/java/crocodile8008/rxutils/android/RxAndroidThreading.kt
@@ -2,23 +2,37 @@
 
 package crocodile8008.rxutils.android
 
+import crocodile8008.rxutils.fromIo
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Maybe
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.schedulers.Schedulers
 
 fun <T : Any> Single<T>.fromIoToMain(): Single<T> = this
-    .subscribeOn(Schedulers.io())
-    .observeOn(AndroidSchedulers.mainThread())
+    .fromIo()
+    .toMain()
 
 fun <T : Any> Observable<T>.fromIoToMain(): Observable<T> = this
-    .subscribeOn(Schedulers.io())
-    .observeOn(AndroidSchedulers.mainThread())
+    .fromIo()
+    .toMain()
 
 fun Completable.fromIoToMain(): Completable = this
-    .subscribeOn(Schedulers.io())
-    .observeOn(AndroidSchedulers.mainThread())
+    .fromIo()
+    .toMain()
+
+fun <T : Any> Maybe<T>.fromIoToMain(): Maybe<T> = this
+    .fromIo()
+    .toMain()
 
 fun <T : Any> Observable<T>.toMain(): Observable<T> = this
+    .observeOn(AndroidSchedulers.mainThread())
+
+fun <T : Any> Single<T>.toMain(): Single<T> = this
+    .observeOn(AndroidSchedulers.mainThread())
+
+fun Completable.toMain(): Completable = this
+    .observeOn(AndroidSchedulers.mainThread())
+
+fun <T : Any> Maybe<T>.toMain(): Maybe<T> = this
     .observeOn(AndroidSchedulers.mainThread())
